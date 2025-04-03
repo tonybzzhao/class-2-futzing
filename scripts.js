157c534082d49e8c672895b222f259ffab8777cc
Bingzhi Zhao <bz2746@nyu.edu>
2025-04-03 17:00:47 -0400

    New version 2

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
+// script.js
 $(document).ready(function () {
     $(".question").click(function () {
-      let answer = $(this).next(".answer")
-      let toggle = $(this).find(".toggle")
+      let answer = $(this).next(".answer");
+      let toggle = $(this).find(".toggle");
   
       if (answer.is(":visible")) {
-        answer.slideUp()
-        toggle.text("+")
+        // If the answer is already visible, slide it up and set the toggle to '+'
+        answer.slideUp();
+        toggle.text("+");
       } else {
-        $(".answer").slideUp()
-        $(".toggle").text("+")
-        answer.slideDown()
-        toggle.text("-")
+        // Otherwise, close all answers, reset all toggles, then open this one and set its toggle to '-'
+        $(".answer").slideUp();
+        $(".toggle").text("+");
+        answer.slideDown();
+        toggle.text("-");
       }
-    })
-  })  
+    });
+  });
