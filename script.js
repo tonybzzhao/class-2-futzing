c398625ac99d91337774dce2ad80ce005882452a
Bingzhi Zhao <bz2746@nyu.edu>
2025-04-03 17:24:22 -0400

    Fun Quiz 2

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 // script.js
 $(document).ready(function () {
-    $(".question").click(function () {
-      let answer = $(this).next(".answer");
-      let toggle = $(this).find(".toggle");
+  // Initialize the Leaflet map with a default view (centered on Paris)
+  var map = L.map('map').setView([48.8566, 2.3522], 4);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
   
-      if (answer.is(":visible")) {
-        // If the answer is already visible, slide it up and set the toggle to '+'
-        answer.slideUp();
-        toggle.text("+");
-      } else {
-        // Otherwise, close all answers, reset all toggles, then open this one and set its toggle to '-'
-        $(".answer").slideUp();
-        $(".toggle").text("+");
-        answer.slideDown();
-        toggle.text("-");
+  var marker; // This will hold our location marker
+
+  $(".question").click(function () {
+    // Get the corresponding answer and toggle element
+    let answer = $(this).next(".answer");
+    let toggle = $(this).find(".toggle");
+
+    if (answer.is(":visible")) {
+      // If the answer is already visible, slide it up and reset the toggle
+      answer.slideUp();
+      toggle.text("+");
+    } else {
+      // Close all answers and reset toggles
+      $(".answer").slideUp();
+      $(".toggle").text("+");
+      
+      // Open the clicked answer and change the toggle to indicate open state
+      answer.slideDown();
+      toggle.text("-");
+
+      // Get latitude and longitude from data attributes
+      let lat = $(this).data("lat");
+      let lng = $(this).data("lng");
+      if (lat && lng) {
+        // Recenter the map and update the marker
+        map.setView([lat, lng], 10);
+        if (marker) {
+          marker.setLatLng([lat, lng]);
+        } else {
+          marker = L.marker([lat, lng]).addTo(map);
+        }
       }
-    });
+    }
   });
+});
