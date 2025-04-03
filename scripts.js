c7f9f3d8166084e0c4db5d014eae806ca7ae2a85
Bingzhi Zhao <bz2746@nyu.edu>
2025-04-03 16:46:37 -0400

    New version

--- scripts.js
+++ scripts.js
@@ -1,48 +1,16 @@
-function injectImage() {
-    // Create an image element
-    const img = document.createElement("img")
-
-    // Set the image source and alt text
-    img.src =
-        "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Out_of_the_Vein.jpg/220px-Out_of_the_Vein.jpg"
-    img.alt = "Sample Image"
-
-    // Find the target div and add the image to it
-    const container = document.getElementById("right-panel")
-
-    container.innerHTML = '';
-    container.appendChild(img)
-}
-
-function injectImage2() {
-    // Create an image element
-    const img = document.createElement("img")
-
-    // Set the image source and alt text
-    img.src =
-        "https://upload.wikimedia.org/wikipedia/en/thumb/9/94/Sublime_Self-Titled.jpg/220px-Sublime_Self-Titled.jpg"
-    img.alt = "Sample Image"
-
-    // Find the target div and add the image to it
-    const container = document.getElementById("right-panel")
-
-    container.innerHTML = '';
-    container.appendChild(img)
-}
-
-function injectImage3() {
-    console.log('2')
-    // Create an image element
-    const img3 = document.createElement("img")
-
-    // Set the image source and alt text
-    img3.src =
-        "https://upload.wikimedia.org/wikipedia/en/4/4b/Green_Day_-_Dookie_cover.jpg"
-    img3.alt = "Sample Image"
-
-    // Find the target div and add the image to it
-    const container = document.getElementById("right-panel")
-
-    container.innerHTML = '';
-    container.appendChild(img3)
-}  
+$(document).ready(function () {
+    $(".question").click(function () {
+      let answer = $(this).next(".answer")
+      let toggle = $(this).find(".toggle")
+  
+      if (answer.is(":visible")) {
+        answer.slideUp()
+        toggle.text("+")
+      } else {
+        $(".answer").slideUp()
+        $(".toggle").text("+")
+        answer.slideDown()
+        toggle.text("-")
+      }
+    })
+  })
